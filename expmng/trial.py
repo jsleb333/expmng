582caad71a3529d8bcfe05c8b915b38036f4ddca
jsleb333 <jean-samuel.leboeuf.1@ulaval.ca>
2018-11-10 15:15:29 -0500

    bug fix and various changes of mind

--- expmng/trial.py
+++ expmng/trial.py
@@ -9,23 +9,30 @@ class Trial:
 
     At initialization, the parent experiment should be provided as well as all parameters of the trial. An implementation of the method 'procedure' should be given, either by subclassing this class or by assigning a function to replace the method. The trial is launch by calling the 'run' method. The procedure method should take arguments for all trial parameters passed in the __init__ as well as a checkpoint filename and a log filename.
     """
-    checkpoint_extension = '.ckpt'
-    log_extension = '.log'
+    checkpoint_filename = 'checkpoint'
+    log_filename = 'log'
+    params_filename = 'trial_params'
 
-    def __init__(self, experiment, trial_name=None, **trial_params):
+    def __init__(self, experiment, trial_name=None,
+                 checkpoint_extension='.pkl', log_extension='.csv',
+                 **trial_params):
         self.exp_name = experiment.exp_name
         self.trial_name = trial_name or self._generate_trial_name(experiment)
-        self.trial_params = trial_params
         self.trial_path = os.path.join(experiment.path, self.trial_name)
+
+        self.checkpoint_extension = checkpoint_extension
+        self.log_extension = log_extension
+
+        self.trial_params = trial_params
 
         os.makedirs(self.trial_path)
 
     @property
-    def checkpoint_filename(self):
-        return os.path.join(self.trial_path, self.trial_name + self.checkpoint_extension)
+    def checkpoint_filepath(self):
+        return os.path.join(self.trial_path, self.checkpoint_filename + self.checkpoint_extension)
     @property
-    def log_filename(self):
-        return os.path.join(self.trial_path, self.trial_name + self.log_extension)
+    def log_filepath(self):
+        return os.path.join(self.trial_path, self.log_filename + self.log_extension)
 
     def _generate_trial_name(self, experiment):
         """
@@ -48,7 +55,8 @@ class Trial:
 
     def save_trial(self):
         json_file = self._generate_json_file()
-        with open(self.trial_path + '/trial_params.json', 'w')
+        with open(self.trial_path + self.params_filename + '.json', 'w') as file:
+            json.dump(json_file, file, sort_keys=True)
 
     def _generate_json_file(self):
         pass
